7246479c11b6923513aaae38381ba5504c9bbfc5
Saikarthik143 <59730736+Saikarthik143@users.noreply.github.com>
2020-10-04 14:37:28 +0530

    Create action.js

---
+++ action.js
@@ -0,0 +1,48 @@
+import { USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNIN_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL,USER_UPDATE_REQUEST,USER_UPDATE_SUCCESS,USER_UPDATE_FAIL  } from "../constants/userConstants";
+import Axios from "axios";
+import Cookie from 'js-cookie';
+const signin = (userName, password) => async (dispatch) => {
+    dispatch({ type: USER_SIGNIN_REQUEST, payload: { userName, password } });
+    try {
+     // const { data } = await Axios.get('http://localhost:3000/Users?userName='+userName+'&password='+password, { userName, password });
+     
+     const dataa=Axios.get('http://localhost:3000/Users?userName='+userName+'&password='+password).then(Response=>{
+                if(Response.data.length==1){
+                  localStorage.setItem('userName',Response.data[0].userName);
+                 localStorage.setItem('accountType',Response.data[0].account);
+            localStorage.setItem('idm',Response.data[0].idm);
+            localStorage.setItem('id',Response.data[0].id);
+                   console.log(Response.data);
+                }
+                else{
+                    alert('invalid credentials');
+                }
+            })
+           .catch(err=>{
+               alert('invalid')
+               console.log(err)
+           }
+           )
+
+     dispatch({ type: USER_SIGNIN_SUCCESS, payload: dataa });
+      Cookie.set('userInfo', JSON.stringify(dataa));
+    } catch (error) {
+      dispatch({ type: USER_SIGNIN_FAIL, payload: error.message });
+    }
+}
+
+  const register = (arr) => async (dispatch) => {
+    dispatch({ type: USER_REGISTER_REQUEST, payload: arr  });
+    try {
+      const { data } = await Axios.post('http://localhost:3000/Users',  arr );
+      console.log(data);
+      dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+      Cookie.set('userInfo', JSON.stringify(data));
+    } catch (error) {
+      dispatch({ type: USER_REGISTER_FAIL, payload: error.message });
+    }
+  }
+
+ 
+  
+  export { signin, register };
