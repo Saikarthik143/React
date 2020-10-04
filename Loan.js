b261b72088925bda0daa4062d7d7d643b2cf3053
Saikarthik143 <59730736+Saikarthik143@users.noreply.github.com>
2020-10-04 12:56:07 +0530

    Add files via upload

--- Loan.js
+++ Loan.js
@@ -3,9 +3,11 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import './loan.css';
+import bank from './bank.png';
 
 const Loan=()=>{
-    let [accountHolderName,setAccountHolderName]=useState('');
+    let [accountHolderName,setAccountHolderName]=useState(localStorage.getItem('userName'));
     const [loanType,setLoanType]=useState('');
     const[loanAmount,setLoanAmount]=useState('');
     const [loanApplyDate,setLoanApplyDate]=useState('');
@@ -16,7 +18,45 @@ const Loan=()=>{
     const[loanAmountErr,setLoanAmountErr]=useState({});
     const[loanApplyDateErr,setLoanApplyDateErr]=useState({});
     const[ROIErr,setROIErr]=useState({});
-    const[duriationErr,setDuriationErr]=useState({})
+    const[duriationErr,setDuriationErr]=useState({});
+    let[selectedVal,setSelectedVal]=useState('');
+    const[annualIncome,setAnnualIncome]=useState('');
+    const[companyName,setCompanyName]=useState('');
+    const[designation,setDesignation]=useState('');
+    const[totalExp,setTotalexp]=useState('');
+    const[currentExp,setCurrentExp]=useState('');
+    const[annualIncomeErr,setAnnualIncomeErr]=useState({});
+    const[companyNameErr,setCompanyNameErr]=useState({});
+    const[designationErr,setDesignationErr]=useState({});
+    const[totalExpErr,setTotalexpErr]=useState({});
+    const[currentExpErr,setCurrentExpErr]=useState({});
+    const[courseFee,setCourseFee]=useState('');
+    const[Course,setCourse]=useState('');
+    const[fatherName,setFatherName]=useState('');
+    const[fatherOccupation,setFatherOccupation]=useState('');
+    const[courseFeeErr,setCourseFeeErr]=useState({});
+    const[CourseErr,setCourseErr]=useState({});
+    const[fatherNameErr,setFatherNameErr]=useState({});
+    const[fatherOccupationErr,setFatherOccupationErr]=useState({});
+    let id=localStorage.getItem('id');
+    const array1={
+        id,
+        accountHolderName,
+       
+        loanAmount,
+        selectedVal,
+        loanApplyDate,
+        ROI,
+        duriation,
+        fatherName,
+        fatherOccupation,
+        Course,
+        courseFee,
+        annualIncome,
+        companyName,
+        totalExp,
+        currentExp
+    }
 
 
 
@@ -26,6 +66,13 @@ const Loan=()=>{
         if(isValid){
             alert('success');
             console.log("submitted");
+            axios.post('http://localhost:3000/loan',array1).then(Response=>{
+                console.log(Response)
+                
+            })
+            .catch(error=>{
+                console.log(error)
+            })
         
         }
         else
@@ -35,7 +82,9 @@ const Loan=()=>{
         console.log(accountHolderName);
             
 
-
+        const handleChange=(event)=>{
+            setSelectedVal(event.target.value);
+        }
 
         const dateChange=(loanApplyDate)=>{
             setLoanApplyDate(loanApplyDate);
@@ -44,20 +93,26 @@ const Loan=()=>{
         const formValidation=()=>{
             let isValid=true;
            // const accountHolderNameErr={};
-            const loanTypeErr={};
+            //const loanTypeErr={};
             const loanAmountErr={};
             const loanApplyDateErr={};
             const ROIErr={};
-            const duriationErr={}
+            const duriationErr={};
+            const annualIncomeErr={};
+           // const duriationErr={};
+            const courseFeeErr={};
+            const CourseErr={};
+            const fatherNameErr={};
+            const fatherOccupationErr={};
+            const totalExpErr={};
+            const currentExpErr={};
+            const designationErr={};
+            const companyNameErr={};
           
 
 
 
-            if(!loanType){
-                isValid=false;
-                loanTypeErr.noloan="please select the type";
-
-            }
+          
             if(!loanAmount){
                 loanAmountErr.noamount="amount not mentioned";
             }
@@ -75,6 +130,41 @@ const Loan=()=>{
            if(!ROI){
                ROIErr.norate="mention it";
            }
+           if(!courseFee){
+            courseFeeErr.norate="mention it";
+        }
+        if(!Course){
+            CourseErr.norate="mention it";
+        }
+        if(!fatherName){
+            fatherNameErr.norate="mention it";
+        }
+        if(!fatherOccupation){
+            fatherOccupationErr.norate="mention it";
+        }
+        if(!annualIncome){
+            annualIncomeErr.norate="mention it";
+        }
+        if(!companyName){
+            companyNameErr.norate="mention it";
+        }
+        if(!designation){
+            designationErr.norate="mention it";
+        }
+        if(!totalExp){
+            totalExpErr.norate="mention it";
+        }
+        if(!currentExp){
+            currentExpErr.norate="mention it";
+        }
+
+
+
+
+
+
+
+
            if(!duriation){
                duriationErr.notime="mention";
            }
@@ -82,7 +172,16 @@ const Loan=()=>{
            setLoanAmountErr(loanAmountErr);
            setLoanApplyDateErr(loanApplyDateErr);
            setROIErr(ROIErr);
-           setLoanTypeErr(loanTypeErr);
+           //setLoanTypeErr(loanTypeErr);
+           setAnnualIncomeErr(annualIncomeErr);
+           setCourseErr(CourseErr);
+           setCourseFeeErr(courseFeeErr);
+           setFatherOccupationErr(fatherOccupationErr);
+           setFatherNameErr(fatherNameErr);
+           setCompanyNameErr(companyNameErr);
+           setDesignationErr(designationErr);
+           setTotalexpErr(totalExpErr);
+           setCurrentExpErr(currentExpErr);
            return isValid;
 
         }
@@ -94,26 +193,30 @@ const Loan=()=>{
 
 
     return <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-danger">
+        <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor:"#fa983a"}}>
     <div className="col">
         <ul className="nav row nav pills">
             <li className="col-sm-3 nav-item text-center">
-               <Link className="nav-link btn-outline-warning"  > <i className="fa fa-fw fa-home"></i>Home</Link>
+            <Link to={{pathname:'/Demo'}}>
+                    <img className="card-img-top" src={bank} alt="card" style={{width:"100px",height:"50px"}}/>
+                   
+                        </Link>
             </li>
-            <li class="col-sm-3 nav-item text-center">
-                <Link class="nav-link btn-outline-warning"   ><i className="fa fa-search"></i>Search</Link>
+            <li class="col-sm-3 nav-item text-center" style={{fontFamily:"David Libre",fontWeight:"bold"}}>
+                <Link className="nav-link btn-outline-light"   ><i className="fa fa-search"></i>Search</Link>
             </li>
             <li className="col-sm-3 nav-item text-center">
-                <Link className="nav-link btn-outline-warning" ><i className="fa fa-fw fa-user "></i>ViewProfile</Link>
+                <Link className="nav-link btn-outline-light" style={{fontFamily:"David Libre",fontWeight:"bold"}} ><i className="fa fa-fw fa-user "></i>ViewProfile</Link>
             </li>
             <li className="col-sm-3 nav-item text-center">
             <Link to={{pathname:'/Login',hash:'#Login'}}>
-                 <button className="btn btn-outline-warning" onClick={Logout}><i className="fa  fa-sign-out"></i>Logout </button> 
+                 <button className="btn btn-outline-light" style={{fontFamily:"David Libre",fontWeight:"bold"}} onClick={Logout}><i className="fa  fa-sign-out"></i>Logout </button> 
                </Link>
             </li>
         </ul>
     </div>
 </nav>
+   
 <div className="bgclr">
          <div className="container">
         <div className="row">
@@ -134,14 +237,94 @@ const Loan=()=>{
         </div>
         <div className="form-group">
             <label htmlFor="loanType">Loan Type</label>
-            <select onChange={(e)=>setLoanType(e.target.value)}>
-                <option value=""></option>
-                <option value="Personal">Personal</option>
+            <select value={selectedVal} onChange={handleChange}>
+                <option value="none">Select Option</option>
+                <option value="Personal/Home Loan">Personal/Home Loan</option>
                 <option value="Education">Education</option>
             </select>
-            {Object.keys(loanTypeErr).map((key)=>{
-                return <div key={key} style={{color:"red"}}>{loanTypeErr[key]}</div>
+         
+            {
+                selectedVal==='Personal/Home Loan'?<div><form>
+                     <div className="form-group">
+            <label htmlFor="annualIncome">Annual Income</label>
+            <input type="annualIncome" className="form-control" name="annualIncome" id="annualIncome" onChange={(e)=>setAnnualIncome(e.target.value)}></input>
+            {Object.keys(annualIncomeErr).map((key)=>{
+                return <div key={key} style={{color:"red"}}>{annualIncomeErr[key]}</div>
             })}
+        </div>
+        <div className="form-group">
+            <label htmlFor="companyName">Company Name</label>
+            <input type="companyName" className="form-control" name="companyName" id="companyName" onChange={(e)=>setCompanyName(e.target.value)}></input>
+            {Object.keys(companyNameErr).map((key)=>{
+                return <div key={key} style={{color:"red"}}>{companyNameErr[key]}</div>
+            })}
+        </div>
+        <div className="form-group">
+            <label htmlFor="designation">Designation</label>
+            <input type="designation" className="form-control" name="designation" id="designation" onChange={(e)=>setDesignation(e.target.value)}></input>
+            {Object.keys(designationErr).map((key)=>{
+                return <div key={key} style={{color:"red"}}>{designationErr[key]}</div>
+            })}
+        </div>
+        <div className="form-group">
+            <label htmlFor="totalExp">Total Experience</label>
+            <input type="totalExp" className="form-control" name="totalExp" id="totalExp" onChange={(e)=>setTotalexp(e.target.value)}></input>
+            {Object.keys(totalExpErr).map((key)=>{
+                return <div key={key} style={{color:"red"}}>{totalExpErr[key]}</div>
+            })}
+        </div>
+        <div className="form-group">
+            <label htmlFor="currentExp">current Experience</label>
+            <input type="currentExp" className="form-control" name="currentExp" id="currentExp" onChange={(e)=>setCurrentExp(e.target.value)}></input>
+            {Object.keys(currentExpErr).map((key)=>{
+                return <div key={key} style={{color:"red"}}>{currentExpErr[key]}</div>
+            })}
+        </div>
+                </form></div>
+                :selectedVal==='Education'?<div><form>
+                     <div className="form-group">
+            <label htmlFor="courseFee">CourseFee</label>
+            <input type="courseFee" className="form-control" name="courseFee" id="courseFee" onChange={(e)=>setCourseFee(e.target.value)}></input>
+            {Object.keys(courseFeeErr).map((key)=>{
+                return <div key={key} style={{color:"red"}}>{courseFeeErr[key]}</div>
+            })}
+
+        </div>
+        <div className="form-group">
+            <label htmlFor="course">Course</label>
+            <input type="course" className="form-control" name="course" id="course" onChange={(e)=>setCourse(e.target.value)}></input>
+            {Object.keys(CourseErr).map((key)=>{
+                return <div key={key} style={{color:"red"}}>{CourseErr[key]}</div>
+            })}
+            
+        </div>
+        <div className="form-group">
+            <label htmlFor="fatherName">Father Name</label>
+            <input type="fatherName" className="form-control" name="fatherName" id="fatherName" onChange={(e)=>setFatherName(e.target.value)}></input>
+            {Object.keys(fatherNameErr).map((key)=>{
+                return <div key={key} style={{color:"red"}}>{fatherNameErr[key]}</div>
+            })}
+            
+        </div>
+        <div className="form-group">
+            <label htmlFor="fatherOccupation">father Occupation</label>
+            <input type="fatherOccupation" className="form-control" name="fatherOccuption" id="fatherOccuption" onChange={(e)=>setFatherOccupation(e.target.value)}></input>
+            {Object.keys(fatherOccupationErr).map((key)=>{
+                return <div key={key} style={{color:"red"}}>{fatherOccupationErr[key]}</div>
+            })}
+            
+        </div>
+        <div className="form-group">
+            <label htmlFor="annualIncome">Annual Income</label>
+            <input type="annualIncome" className="form-control" name="annualIncome" id="annualIncome" onChange={(e)=>setAnnualIncome(e.target.value)}></input>
+            {Object.keys(annualIncomeErr).map((key)=>{
+                return <div key={key} style={{color:"red"}}>{annualIncomeErr[key]}</div>
+            })}
+            
+        </div>
+                </form></div>:null
+            }
+           
         </div>
         <div className="form-group">
             <label htmlFor="loanAmount">loanAmount</label>
